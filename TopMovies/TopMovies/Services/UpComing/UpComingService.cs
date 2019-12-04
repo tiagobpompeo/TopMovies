193933c0f21a88cb69420fbd48f1ad495beb1a40
Tiago Benaias Pompeo <tiago.benaias.pompeo@everis.com>
2019-12-04 11:05:29 -0300

    Update Xamarin Forms
    Update Xamarin Forms

--- TopMovies/TopMovies/Services/UpComing/UpComingService.cs
+++ TopMovies/TopMovies/Services/UpComing/UpComingService.cs
@@ -25,31 +25,31 @@ namespace TopMovies.Services.UpComing
 
         public async Task<List<Movies.Result>> GetAllMoviesAsync(int page)
         {
-            Analytics.TrackEvent("Evento Teste");
-            Exception exception = new Exception();
-            var properties = new Dictionary<string, string> {
-                { "Category", "Music" },
-                { "Wifi", "On" }
-              };
-            Crashes.TrackError( exception, properties);
-            Crashes.GenerateTestCrash();
-         
-            //List<Movies.Result> moviesFromCache = await GetFromCache<List<Movies.Result>>(page.ToString());
-            //if (moviesFromCache != null)//loaded from cache
-            //{
-            //    return moviesFromCache;
-            //}
-            //else
-            //{
-                                  
+            //Analytics.TrackEvent("Evento Teste");
+            //Exception exception = new Exception();
+            //var properties = new Dictionary<string, string> {
+            //    { "Category", "Music" },
+            //    { "Wifi", "On" }
+            //  };
+            //Crashes.TrackError( exception, properties);
+            //Crashes.GenerateTestCrash();
+
+            List<Movies.Result> moviesFromCache = await GetFromCache<List<Movies.Result>>(page.ToString());
+            if (moviesFromCache != null)//loaded from cache
+            {
+                return moviesFromCache;
+            }
+            else
+            {
+
 
                 string uri = $"{ApiConstants.BaseApiUrl}upcoming?api_key={ApiConstants.Api_key}&language=en-us&page={page}";
                 var movies = await _genericRepository.GetAsync<Movies>(uri);
                 GeneralVar.TotalPages = movies.Total_pages;//count
                 var listMovies = movies.Results;
-                //Cache.InsertObject(page.ToString(), listMovies, DateTimeOffset.Now.AddSeconds(20));
+                Cache.InsertObject(page.ToString(), listMovies, DateTimeOffset.Now.AddSeconds(20));
                 return listMovies;
-            //}      
+            }      
         }
 
         public async Task<GenreClass> GetAllGenresAsync()
